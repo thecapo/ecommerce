4a1719ae4d8c68528bc94d655859b8651e76d499
Carlo Nino Mabalatan <the7thcapo@gmail.com>
2025-10-24 01:32:16 +0000

    Add pages

---
+++ app/pages/sales/index.tsx
@@ -0,0 +1,5 @@
+export default function SalesPage() {
+    return (
+        <div>Sales Page</div>
+    )
+}
---
+++ app/pages/stores/index.tsx
@@ -0,0 +1,5 @@
+export default function StoresPage() {
+    return (
+        <div>Stores Page</div>
+    )
+}
