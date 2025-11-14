af638c7bbd7d9d5d482e896057ac88f8e3b4ea87
Carlo Nino Mabalatan <the7thcapo@gmail.com>
2025-11-14 09:27:51 +0000

    Setup user

--- app/pages/cart/page.tsx
+++ app/pages/cart/page.tsx
@@ -3,9 +3,7 @@ import ShoppingCartList from "@/app/pages/cart/ShoppingCartList";
 export const dynamic = 'force-dynamic';
 
 export default async function CartPage() {
-  // const response = await fetch(process.env.NEXT_PUBLIC_SITE_URL + '/api/users/2/cart', {
-  const response = await fetch('https://bug-free-adventure-qjjj64vwj7cjqv-3000.app.github.dev' + '/api/users/2/cart', {
-    
+  const response = await fetch(`${process.env.BASE_URL}` + '/api/users/2/cart', {
     cache: 'no-cache',
   });
   const cartProducts = await response.json();
